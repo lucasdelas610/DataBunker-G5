8cbbe765d64939f189d87ca3ad2b318a466ce2b7
anna <anna.gual@estudiant.enti.cat>
2025-12-20 19:12:10 +0100

    FJ-16
    Guardar la clave en un archivo.

--- key_manager.py
+++ key_manager.py
@@ -1,8 +1,11 @@
 # Gestion de claves de cifrado
 
 from cryptography.fernet import Fernet
+
 def generar_clave(nom_fitxer = "clave.key"):
     clave = Fernet.generate_key()
+    with open (nom_fitxer, "wb") as f:
+        f.write(clave)
     return clave
 clave = generar_clave()
-print(f"Clave guardada")
+print(f"Clave guardada correctamente")
