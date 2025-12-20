45d6d446f7f06765343aff93ea22d875c15bf995
Lucas <lucasdelas610@gmail.com>
2025-12-20 19:09:16 +0100

    FJ-14
    Crear el sistema para generar el sistema de cifrado

--- key_manager.py
+++ key_manager.py
@@ -1,3 +1,8 @@
 # Gestion de claves de cifrado
 
 from cryptography.fernet import Fernet
+def generar_clave(nom_fitxer = "clave.key"):
+    clave = Fernet.generate_key()
+    return clave
+clave = generar_clave()
+print(f"Clave guardada")
