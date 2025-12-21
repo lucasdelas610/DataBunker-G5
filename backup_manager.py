7428ee8043759949d452ab9319e75b9607ef2aee
andreaolivas-cloud <andrea.olivas@estudiant.enti.cat>
2025-12-21 17:04:59 +0100

    FJ-20 Crear la función que restaura una copia a partir del .zip.

--- backup_manager.py
+++ backup_manager.py
@@ -2,3 +2,13 @@
 
 import zipfile
 import os
+
+def restaurar_copia(zip_arxiu, carpeta_desti): #FJ-20
+    if os.path.exists(zip_arxiu): #se comprueba si existe el archivo
+        arxiu= zipfile.ZipFile(zip_arxiu, 'r')
+        arxiu.extractall(carpeta_desti) #se extrae el contenido del archivo
+        arxiu.close()
+        print(f"Restauració completada a {carpeta_desti}")
+    else:
+        print(f"Error, no s'ha trobat l'arxiu '{zip_arxiu}'") #si no existe el archivo, printea error
+
