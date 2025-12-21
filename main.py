effe35cfa05f3702115ffb3d12e5a23c79dabd23
Lena123 <lena.boajram@estudiant.enti.cat>
2025-12-21 17:02:15 +0100

    FJ-18 Crear la función que comprime una carpeta en un archivo .zip.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # Punto de entrada del programa
+import os
+import zipfile
+
+def comprimir_carpeta(carpeta, zip_final):
+    zip_1 = zipfile.ZipFile(zip_final, 'w')
+    for archivo in os.listdir(carpeta):
+        zip_1.write(carpeta + "/" + archivo)
+    zip_1.close()
