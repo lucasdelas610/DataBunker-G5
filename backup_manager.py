cb371f9f3b48eaa63bb0999fcae336f3975d32a0
andreaolivas-cloud <andrea.olivas@estudiant.enti.cat>
2025-12-21 17:07:18 +0100

    FJ-21 Verificar que los archivos restaurados son correctos.

--- backup_manager.py
+++ backup_manager.py
@@ -12,3 +12,15 @@ def restaurar_copia(zip_arxiu, carpeta_desti): #FJ-20
     else:
         print(f"Error, no s'ha trobat l'arxiu '{zip_arxiu}'") #si no existe el archivo, printea error
 
+def verificar_restauracio(carpeta_desti): #FJ-21
+    if os.path.exists(carpeta_desti):
+        fitxers = os.listdir(carpeta_desti)
+        if len(fitxers) > 0: #aqui se comprueban posibles fallos, como si la carpeta puede estar vacia o no existe
+            print(f"Verificació completada, s'han trobat {len(fitxers)} fitxers.")
+            return True
+        else:
+            print("Verificació fallida, la carpeta està buida.")
+            return False
+    else:
+        print("Verificació fallida, la carpeta no existeix.")
+        return False
