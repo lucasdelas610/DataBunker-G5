eac75063d3019c17293f201e678f0e7e5cafabf5
Pol Latorre <latorrecanisapol@gmail.com>
2025-12-21 18:12:48 +0100

    FJ-23 Hacer pruebas básicas para comprobar que todo lo anterior funciona

--- main.py
+++ main.py
@@ -11,3 +11,35 @@ def comprimir_carpeta(carpeta, zip_final):
         zip_1.write(carpeta + "/" + archivo)
     zip_1.close()
 #Si le das a iniciar se guarda ya la copia .zip FJ 19
+
+
+
+# FJ-22
+import key_manager
+import crypto_utils
+
+
+# 1. Pruebo si la clave se genera bien
+print("1. Probando gestor de claves...")
+key_manager.generar_clave() 
+
+# 2. Creo un archivo para ver si cifra bien
+with open("test_secreto.txt", "w") as f:
+    f.write("Esto es una prueba de cifrado")
+# 3. Intento cifrarlo usando mi funcion de crypto_utils
+print("2. Probando a cifrar archivo de prueba")
+
+mi_clave = key_manager.cargar_clave() # Cargo la clave primero
+
+if mi_clave:
+    crypto_utils.cifrar_archivo("test_secreto.txt", mi_clave) # Si tengo clave, intento cifrar
+
+    # 4. Compruebo si ha salido el archivo .enc
+    if os.path.exists("test_secreto.txt.enc"):
+        print("Perfecto: El cifrado funciona, se ha creado el archivo .enc")
+    else:
+        print("Fallo: No se ha creado el archivo cifrado")
+else:
+    print("No se ha podido probar el cifrado porque fallo la clave")
+
+print("Fin de las pruebas")
