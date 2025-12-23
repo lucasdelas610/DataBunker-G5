5ba01f0f96e54e6172e0fa1d1e864c2a2026edc2
Lucas <lucasdelas610@gmail.com>
2025-12-23 12:13:57 +0100

    FJ-24
    Crear función básica de descifrado

--- crypto_utils.py
+++ crypto_utils.py
@@ -28,3 +28,21 @@ def cifrar_archivo(nombre_archivo, clave):
         file.write(datos_encriptados)
         
     print("He creado el archivo cifrado: " + nombre_final)
+
+# FJ-24
+def descifrar_archivo(archivo_encriptado, clave):
+    if os.path.exists(archivo_encriptado) == False: # se mira si existe
+        print("Error: No encuentro el archivo cifrado(.enc)")
+        return None
+        
+    f = Fernet(clave)
+    with open(archivo_encriptado, "rb") as file:
+        datos_encriptados = file.read()
+            
+    datos_originales = f.decrypt(datos_encriptados)#La librería Fernet descifra los datos
+    nombre_salida = archivo_encriptado.replace(".enc", "") # se le quita .enc al nombre para guardarlo
+        
+    with open(nombre_salida, "wb") as file:
+        file.write(datos_originales)
+    print("Archivo descifrado temporalmente: " + nombre_salida)
+    return nombre_salida
