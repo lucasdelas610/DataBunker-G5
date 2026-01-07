f086d7a279b23cc8b2437101c09af4ab350a465c
anna <anna.gual@estudiant.enti.cat>
2026-01-07 12:39:14 +0100

    FJ - 41
    Validar existencia de archivos

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
+import os
 import menu
 
-menu.mostrar_menu()
+
+# FJ - 41: Validacion de los archivos
+
+def validar_archivos():
+    archivos = [
+        "menu.py",
+        "key_manager.py",
+        "crypto_utils.py",
+        "backup_manager.py"
+        ]
+
+    error = 0
+
+    for archivo in archivos:
+        if os.path.exists(archivo) == True:
+            print("Archivos en orden: " + archivo)
+        else:
+            print("Error, faltan archivos:" + archivo)
+            error = error + 1
+
+    if error == 0:
+        return True
+    else:
+        return False
+
+
+if validar_archivos() == True:
+    print("Todo en orden\n")
+    menu.mostrar_menu()
+else:
+    print("Error al iniciar: Faltan archivos importantes")
