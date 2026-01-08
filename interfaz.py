a4f6d5943e2e0a3d24fa755c2df78eaeaa5381e5
Pol Latorre <latorrecanisapol@gmail.com>
2026-01-08 11:59:35 +0100

    Final de la interfaz

--- interfaz.py
+++ interfaz.py
@@ -32,3 +32,49 @@ def descifrar():
     if archivo:
         nombre_salida = crypto_utils.descifrar_archivo(archivo, clave)
         messagebox.showinfo("Hecho", f"Archivo restaurado: {nombre_salida}")
+
+def backup():
+    carpeta = filedialog.askdirectory(title="Selecciona carpeta para hacer backup")  # aqui se elige la carpeta para guardar
+    if carpeta:
+       
+        nombre = simpledialog.askstring("Nombre", "Nombre del archivo backup (ej: copia.zip):")
+        if nombre:
+            # Solo se puede si es .zip
+            if not nombre.endswith(".zip"):
+                nombre += ".zip"
+            
+            resultado = backup_manager.comprimir_carpeta(carpeta, nombre)
+            if resultado:
+                messagebox.showinfo("Bien", "Backup creado correctamente.")
+            else:
+                messagebox.showerror("Error", "Algo fallo (mira historial.txt)")
+
+def restaurar():
+    archivo_zip = filedialog.askopenfilename(title="Elige el ZIP", filetypes=[("Archivos ZIP", "*.zip")])
+    if archivo_zip:
+        destino = filedialog.askdirectory(title="Donde lo descomprimo?")
+        if destino:
+            backup_manager.restaurar_copia(archivo_zip, destino)
+            messagebox.showinfo("Listo", "Restauracion completada.")
+
+
+ventana = tk.Tk()
+ventana.title("Programa Cifrado")
+
+
+ventana.geometry("320x380") # estas son las medidas de la ventana
+
+# Etiqueta titulo
+lbl_titulo = tk.Label(ventana, text="DATA BUNKER", font=("Arial", 16))
+lbl_titulo.pack(pady=10)
+
+tk.Button(ventana, text="1. Generar Clave", command=generar, width=30).pack(pady=10) # Aqui estan todos los botones 
+tk.Button(ventana, text="2. Cifrar Archivo", command=cifrar, width=30).pack(pady=10)
+tk.Button(ventana, text="3. Descifrar Archivo", command=descifrar, width=30).pack(pady=10)
+tk.Button(ventana, text="4. Crear Backup", command=backup, width=30).pack(pady=10)
+tk.Button(ventana, text="5. Restaurar Backup", command=restaurar, width=30).pack(pady=10)
+
+
+tk.Button(ventana, text="Salir", command=ventana.quit, bg="#ffcccc").pack(pady=20) # Con este boton puedes salir del programa
+
+ventana.mainloop() # Bucle principal
